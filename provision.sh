2b95ca75ce032b519f715cc660866ef1c4e7df66
om1chael <omichaeltmichael@live.co.uk>
2021-09-01 10:31:58 +0100

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -1,20 +1,20 @@
-#!/bin/bash
+!#/bin/bash
 
-# Update the sources list
 sudo apt-get update -y
-
-# upgrade any packages available
 sudo apt-get upgrade -y
-
-# install nginx
 sudo apt-get install nginx -y
-
-
-# install npm 
- sudo apt-get install npm -y
-
-sudo apt-get install npm -y
+sudo apt-get install nodejs -y
 sudo apt-get install python-software-properties -y
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install nodejs -y
-
+cd /home/ubuntu/app
+sudo npm install pm2 -g
+sudo npm install
+sudo rm /etc/nginx/sites-available/default
+sudo ln -s /home/ubuntu/config_files/default /etc/nginx/sites-available/default
+sudo nginx -t
+sudo systemctl restart nginx
+echo 'export DB_HOST=192.168.10.150:27017/posts/' >> /etc/environment
+source /etc/environment
+node seeds/seed.js
+npm start
